895761a4a364eadee504ff1c466a75f45e911fb7
Wesley <60309425+Marlborow@users.noreply.github.com>
2023-08-19 00:01:45 +0100

    Update SirrusAssembler.h (strings)

--- SirrusAssembler.h
+++ SirrusAssembler.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <unordered_map>
+#include <cstdint>
 #include <vector>
 #include <string>
 #include <sstream>
@@ -19,35 +20,34 @@ class SirrusAssembler
 
     //variables
     std::unordered_map<std::string, int> registers = {
-        {"eax", 0},
-        {"ebx", 0},
-        {"ecx", 0},
-        {"edx", 0},
+        {"eax",0},
+        {"ebx",0},
+        {"ecx",0},
+        {"edx",0},
     };
 
     int cmp_reg;
 
     std::unordered_map<std::string, int> labels;
-    std::unordered_map<std::string, int> variables;
+    std::unordered_map<std::string, std::vector<int>> variables;
     std::vector<int> retpoints;
     
     
     //functions
+    int evaluateExpression(std::string src);
+    std::string extractVariableFromExpression(const std::string& expression);
     std::vector<std::string> split(const std::string &s, char delimiter);
     std::vector<std::string> removeIndents(const std::vector<std::string> &lines);
     void printRegisterValue(const std::string &regName);
     void debugPrintRegisters();
     bool isNumber(const std::string & str);
 
-    int cmd_var    (std::string varName, std::string value);
+    int cmd_var     (std::string varName, std::string value);
     bool cmd_mov    (std::string dest, std::string src, std::string line, int & ip);
     bool cmd_add    (std::string src1, std::string src2);
     bool cmd_sub    (std::string src1, std::string src2);
     bool cmd_mul    (std::string src1, std::string src2);
-
     void cmd_print  (const std::string & src, std::string line, int ip);
-    int cmd_inc    (std::string src);
-    int cmd_dec    (std::string src);
 
     bool cmd_cmp    (std::string src1,std::string src2);
     bool cmd_jmp    (std::string label, int & ip);
